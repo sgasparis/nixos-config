6c6b8774dadf2a675bce11dc0e70026203afe7ce
sgasparis <gasparis.steven@proton.me>
2026-05-08 21:15:40 -0400

    new niri keybinds

--- home.nix
+++ home.nix
@@ -17,4 +17,25 @@
       pull.rebase = false;
     };
   };
+
+  programs.niri = {
+    enable = true;
+    settings = {
+      outputs."*".scale = 1.0;
+    
+      binds = with config.lib.niri.actions; {
+        "Super+T".action = spawn "alacritty";
+        "Super+R".action = spawn "wofi" "--show" "run";
+        "Super+Q".action = close-window;
+        "Super+Shift+E".action = quit;
+        "Super+F".action = fullscreen-window;
+        "Super+Left".action = focus-column-left;
+        "Super+Right".action = focus-column-right;
+        "Super+Up".action = focus-workspace-up;
+        "Super+Down".action = focus-workspace-down;
+        "Super+Shift+Left".action = move-column-left;
+        "Super+Shift+Right".action = move-column-right;
+      };
+    };
+  };
 }
